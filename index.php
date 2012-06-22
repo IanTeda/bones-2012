be2e04ab94e2a7c06d0a45f466f15b74617ef100
Ian Teda <ian@teda.id.au>
2012-06-22 23:15:25 +1000

    Added article styles & jcarousel
    Added styling and dummy content for articles and the

--- index.php
+++ index.php
@@ -1,71 +1,41 @@
 <?php get_header(); ?>		
-	<div id="content">
-		<div id="inner-content" class="wrap clearfix">
-			<p class="description text-center"><?php bloginfo('description'); ?></p>
+<div id="content">
+	<div id="inner-content" class="wrap clearfix">
+		<p class="description text-center"><?php bloginfo('description'); ?></p>
 			
-			<div id="main" class="eightcol first clearfix" role="main">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<h3>Latest Posts</h3>
-					    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						    <header class="article-header">
-							
-							    <h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							
-							    <p class="meta"><?php _e('Posted', 'bonestheme'); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time> <?php _e('by', 'bonestheme'); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e('filed under', 'bonestheme'); ?> <?php the_category(', '); ?>.</p>
-						
-						    </header> <!-- end article header -->
+		<div id="main" class="eightcol first clearfix" role="main">
+			<h3 class="underlined">Latest Posts</h3>
+			    <ul id="mycarousel" class="latest-carousel">
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    	<li>
+                           	<a href="#">
+								<img class="resizeable" src="<?php echo get_template_directory_uri(); ?>/library/images/altered-full-1-220x140.jpg">
+                                <h5 class="article-title text-center">Altered</h5><span class="article-categories text-center">design / illustration</span>
+                            </a>
+						</li><!-- end post li -->
+					<?php endwhile; ?>	
+				</ul>
 					
-						    <section class="post-content clearfix">
-							    <?php //the_content(); ?>
-						    </section> <!-- end article section -->
-						
-						    <footer class="article-footer">
-
-    							<p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
-
-						    </footer> <!-- end article footer -->
-						    
-						    <?php // comments_template(); // uncomment if you want to use them ?>
+				<?php else : ?>
+					<article id="post-not-found" class="hentry clearfix">
+				    	<header class="article-header">
+				        	<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
+				       	</header>
+				        <section class="post-content">
+				        	<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
+				        </section>
+				        <footer class="article-footer">
+				            <p><?php _e("This is the error message in the index.php template.", "bonestheme"); ?></p>
+				        </footer>
+				    </article>
 					
-					    </article> <!-- end article -->
-					
-					    <?php endwhile; ?>	
-					
-					        <?php if (function_exists('bones_page_navi')) { // if experimental feature is active ?>
-						
-						        <?php bones_page_navi(); // use the page navi function ?>
-						
-					        <?php } else { // if it is disabled, display regular wp prev & next links ?>
-						        <nav class="wp-prev-next">
-							        <ul class="clearfix">
-								        <li class="prev-link"><?php next_posts_link(_e('&laquo; Older Entries', 'bonestheme')) ?></li>
-								        <li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', 'bonestheme')) ?></li>
-							        </ul>
-						        </nav>
-					        <?php } ?>		
-					
-					    <?php else : ?>
-					    
-					        <article id="post-not-found" class="hentry clearfix">
-					            <header class="article-header">
-					        	    <h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-					        	</header>
-					            <section class="post-content">
-					        	    <p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-					        	</section>
-					        	<footer class="article-footer">
-					        	    <p><?php _e("This is the error message in the index.php template.", "bonestheme"); ?></p>
-					        	</footer>
-					        </article>
-					
-					    <?php endif; ?>
+				<?php endif; ?>
 			
-				    </div> <!-- end #main -->
+			</div> <!-- end #main -->
     
-				    <?php //get_sidebar(); // sidebar 1 ?>
+			<?php //get_sidebar(); // sidebar 1 ?>
 				    
-				</div> <!-- end #inner-content -->
-    
-			</div> <!-- end #content -->
+	</div> <!-- end #inner-content -->
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>

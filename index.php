1df97aca4e92ef604439748eb3daf6f99881f2ce
Ian Teda <ian@teda.id.au>
2012-06-25 14:57:51 +1000

    Added Goodreads functionality
    Added Goodreads functionality

--- index.php
+++ index.php
@@ -66,45 +66,17 @@
 				
 			<div id="lastfm" class="clearfix">
 				<h3 class="underlined">Album Chart</h3>
-				<div id="lastfmrecords"></div>
+				<div id="lastfmrecords">
+				</div>
 			</div><!-- End of lastfm list -->
 			
 
 			<div id="goodreads" class=" clearfix">
 				<h3 class="underlined">Bookself</h3>
 				<ul class="image-matrix">
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1333561518m/13497.jpg" alt="A Feast for Crows (A Song of Ice and Fire #4) by George R.R. Martin">
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1266648176m/6342483.jpg" alt="Avempartha (The Riyria Revelations, #2) by Michael J.  Sullivan" class="hoverZoomLink">
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1329188100m/62291.jpg" alt="A Storm of Swords (A Song of Ice and Fire, #3) by George R.R. Martin">
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1328416621m/10572.jpg" alt="A Clash of Kings (A Song of Ice and Fire, #2) by George R.R. Martin">
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1330834644m/13496.jpg" alt="A Game of Thrones (A Song of Ice and Fire, #1) by George R.R. Martin" class="hoverZoomLink">
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<img class="book-cover" src="http://photo.goodreads.com/books/1312541516m/12174312.jpg" alt="Percepliquis (The Riyria Revelations, #6) by Michael J.  Sullivan" class="hoverZoomLink">
-						</a>
-					</li>
-				</ul><!-- End of Goodreads list -->
-			</div>
+					<?php if(function_exists('bookshelf')) { bookshelf(); } ?>
+				</ul>
+			</div><!-- End of Goodreads -->
 				    
 	</div> <!-- end #inner-content -->
 </div> <!-- end #content -->

3307303d1428fe57ea51bf775f963d123830a1ed
Ian Teda <ian@teda.id.au>
2012-06-25 16:30:46 +1000

    Added footer html
    Added html for footer styling.

--- footer.php
+++ footer.php
@@ -1,16 +1,54 @@
 			<footer class="footer" role="contentinfo">
-			
 				<div id="inner-footer" class="wrap clearfix">
-					
 					<nav role="navigation">
     					<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 	                </nav>
-	                		
-					<p class="attribution">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-				
+					
+					<blockquote id="footer-quote">
+						<div id="quotescollection_randomquote-0" class="quotescollection_randomquote">
+							<p>
+								<q>If you want others to be happy, practice compassion. If you want to be happy, practice compassion</q> 
+								<cite>&mdash;&nbsp;<span class="quotescollection_author">Dalai Lama</span></cite>
+							</p>
+						</div>
+					</blockquote>
+					
+					<div id="ss-social-links-2" class="widget ss-social-links">
+						<span class="widget-title">Stay Connected</span>
+						<ul class="social-links">
+							<li class="dribbble">
+								<a href="#" target="_blank">Dribbble</a>
+							</li>
+							<li class="facebook">
+								<a href="#" target="_blank">Facebook</a>
+							</li>
+							<li class="skype">
+								<a href="#" target="_blank">Skype</a>
+							</li>
+							<li class="twitter">
+								<a href="http://twitter.com/#" target="_blank">Twitter</a>
+							</li>
+							<li class="vimeo">
+								<a href="#" target="_blank">Vimeo</a>
+							</li>
+							<li class="youtube">
+								<a href="#" target="_blank">Youtube</a>
+							</li>
+						</ul>
+					</div><!-- end .social-links -->
 				</div> <!-- end #inner-footer -->
 				
 			</footer> <!-- end footer -->
+			
+			<footer class="footer-bottom" role="contentinfo">
+				<div id="inner-footer" class="wrap clearfix">
+					<ul>
+						<li>SmartStart © 2012</li>
+						<li><a href="#">Legal Notice</a></li>
+						<li><a href="#">Terms</a></li>
+					</ul>
+				</div> <!-- end #inner-footer -->	
+			</footer> <!-- end footer-bottom -->
 		
 		</div> <!-- end #container -->
 		

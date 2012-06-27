12ff50f165eafbee10c80fc786983d3fd2f4180d
Ian Teda <ian@teda.id.au>
2012-06-27 11:38:39 +1000

    Archive time
    Time to start working on the post archive.

--- archive.php
+++ archive.php
@@ -96,7 +96,7 @@
 			
     				</div> <!-- end #main -->
     
-	    			<?php get_sidebar(); // sidebar 1 ?>
+	    			<?php //get_sidebar(); // sidebar 1 ?>
                 
                 </div> <!-- end #inner-content -->
                 

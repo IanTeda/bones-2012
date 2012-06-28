88fb43fac541e4f902f21457099b8991d70205a4
Ian Teda <ian@teda.id.au>
2012-06-28 21:39:40 +1000

    481up Main Nav Menu
    Working on the main nav menu for 481 up

--- header.php
+++ header.php
@@ -7,45 +7,46 @@
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><html <?php language_attributes(); ?> class="no-js"><![endif]-->
 	
-	<head>
-		<meta charset="utf-8">
+<head>
+	<meta charset="utf-8">
 		
-		<title><?php wp_title(''); ?></title>
+	<title><?php wp_title(''); ?></title>
 		
-		<!-- Google Chrome Frame for IE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<!-- Google Chrome Frame for IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		
-		<!-- mobile meta (hooray!) -->
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!-- mobile meta (hooray!) -->
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
-		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+	<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 				
-  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		
-		<!-- wordpress head functions -->
-		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
+	<!-- wordpress head functions -->
+	<?php wp_head(); ?>
+	<!-- end of wordpress head -->
 			
-		<!-- drop Google Analytics Here -->
-		<!-- end analytics -->
-	</head>
+	<!-- drop Google Analytics Here -->
+	<!-- end analytics -->
+</head>
 	
-	<body <?php body_class(); ?>>
-		<div id="container">
-			<header class="header" role="banner">
-				<div id="inner-header" class="wrap clearfix">
-					<h1 class="logo"><a href="<?php echo home_url(); ?>" rel="nofollow">Ian <span>Teda</span></a></h1>
+<body <?php body_class(); ?>>
+	<div id="container">
+		<header class="header" role="banner">
+			<div id="inner-header" class="wrap clearfix">
 
-					<nav role="navigation">
-						<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-					</nav>
-				</div> <!-- end #inner-header -->
-			</header> <!-- end header -->
+				<nav id="main-nav" role="navigation">
+					<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+				</nav>
+                    
+				<h1 class="logo"><a href="<?php echo home_url(); ?>" rel="nofollow">Ian <span>Teda</span></a></h1>
+			</div> <!-- end #inner-header -->
+		</header> <!-- end header -->
 			
-			<div id="header-dropdown-menu" class="wrap clearfix">
-				<?php bones_main_nav_dropdown(); // Adjust using Menus in Wordpress Admin ?>
-			</div>
+		<div id="main-nav-dropdown" class="wrap clearfix" role="navigation">
+			<?php bones_main_nav_dropdown(); // Adjust using Menus in Wordpress Admin ?>
+		</div><!-- end main-nav-dropdown -->
 

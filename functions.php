953fad8768de20a7a7679ecedb1c8ad37a432b52
Ian Teda <ian@teda.id.au>
2012-06-29 20:26:50 +1000

    Nivo slider not work
    Going to try another slider

--- functions.php
+++ functions.php
@@ -240,8 +240,25 @@ function load_nivo_slider(){
 	wp_enqueue_script( 'nivo-slider' ); ?>
 	
 	<script type="text/javascript">
-		$(window).load(function() {
-    		$('#slider').nivoSlider();
+		jQuery(window).load(function() {
+    		jQuery('#slider').nivoSlider({
+				effect: 'slideInLeft', 			// Specify sets like: 'fold,fade,sliceDown'
+				slices: 15, 				// For slice animations
+		        boxCols: 8, 				// For box animations
+		        boxRows: 4, 				// For box animations
+		        animSpeed: 500, 			// Slide transition speed
+		        pauseTime: 3000, 			// How long each slide will show
+		        startSlide: 0, 				// Set starting Slide (0 index)
+		        directionNav: true, 		// Next & Prev navigation
+		        directionNavHide: true, 	// Only show on hover
+		        controlNav: true, 			// 1,2,3... navigation
+		        controlNavThumbs: false, 	// Use thumbnails for Control Nav
+		        pauseOnHover: true, 		// Stop animation while hovering
+		        manualAdvance: false, 		// Force manual transitions
+		        prevText: 'Prev', 			// Prev directionNav text
+		        nextText: 'Next', 			// Next directionNav text
+        		randomStart: false, 		// Start on a random slide	
+			});
 		});
 	</script><?php
 }
--- index.php
+++ index.php
@@ -5,22 +5,21 @@
 			
 		<div id="main" class="clearfix" role="main">
 		
-			<div id="slider-container">
+			<div class="slider-wrapper theme-default">
+	            <div class="ribbon"></div>
 				<div id="slider" class="nivoSlider">
 					<?php $featured = new WP_Query("category_name=featured&showposts=5"); 
 							while($featured->have_posts()) : $featured->the_post();?>
-									<a href="<?php the_permalink() ?>">
-										<?php feature_image_1140x336(); ?>
-									</a>
+									<a href="<?php the_permalink() ?>"><?php feature_image_1140x336(); ?></a>
 							<?php endwhile; 
 						?>
 				</div><!-- end slider -->
-			</div><!-- end slider-container -->
+ 			</div><!-- end slider-container -->
 		
 		
 		
-			<div id="latest-posts-container">
-				<h3 class="underlined clearfix">Latest Posts</h3>
+			<div id="latest-posts-container" class="clearfix">
+				<h3 class="underlined">Latest Posts</h3>
 			    <ul class="liquid-matrix latest-post-list">
 					<?php $summary = new WP_query('showposts=6'); //Only the latest 3 posts ?>
                 	<?php if (have_posts()) : while ($summary->have_posts()) : $summary->the_post(); ?>

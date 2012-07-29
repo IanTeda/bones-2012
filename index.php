b3b880ba0ea6564b5aa5a6d74ec705c693dc29d9
Ian Teda <ian@teda.id.au>
2012-07-30 09:15:02 +1000

    Tidy Up Index.php styles
    Tidied up the styles for index.php. Not sure if it broke anything yet
    :-)

--- index.php
+++ index.php
@@ -5,19 +5,17 @@ Template Name: Index.php
 */
 ?>
 
-
-<?php // load header.php ?>
-<?php get_header(); ?>
+<?php get_header(); // load header.php ?>
 
 <div id="content">
 	<div id="inner-content" class="wrap clearfix">
-		<div id="main" class="clearfix" role="main">
+		<div id="index" class="clearfix" role="main">
 			
-            <div id="description-wrapper" class="text-center description eightCol pushTwo clearfix">
+            <div id="index-description" class="eightCol pushTwo clearfix">
 				<p><?php bloginfo('description'); ?></p>
             </div>
 			
-			<div id="flexslider-wrapper" class="flexslider-container tenCol pushOne clearfix">
+			<div id="index-flexslider" class="tenCol pushOne clearfix">
 				<div id="slider" class="flexslider">
 					<ul class="slides">
 						<?php $featured = new WP_Query("category_name=featured&showposts=5"); ?>
@@ -33,7 +31,7 @@ Template Name: Index.php
 				</div><!-- end #slider -->
 			</div><!-- end #flexslider-wrapper -->
 		
-			<div id="latest-posts-container" class="twelveCol first clearfix">
+			<div id="index-latest-posts" class="twelveCol first clearfix">
 				<h3 class="index-heading">Latest Posts<span class="index-heading-more"><a href="<?php bloginfo('url'); ?>/sitemap#sitemap-posts-wrapper">Check out more posts &#8230;</a></span></h3>
 
 			    <ul id="latest-post-list" class="twelveCol first">
@@ -52,7 +50,7 @@ Template Name: Index.php
 				</ul>
 			</div><!-- end #latest-posts-container -->
 			
-			<div id="post-categories" class="twelveCol first clearfix">
+			<div id="index-post-categories" class="twelveCol first clearfix">
 				<h3 class="index-heading">Categories</h3>
 				<ul id="post-category-list">
 					<li>
@@ -100,53 +98,53 @@ Template Name: Index.php
 			    </article>
 			<?php endif; ?>
     
-			<div id="row-four-container" class="twelveCol first clearfix">
-				<div id="popular-posts-container" class="sixCol first clearfix">
+			<div id="index-row-four" class="twelveCol first clearfix">
+				<div id="index-popular-posts" class="sixCol first clearfix">
 					<h3 class="index-heading">Popular Posts<span class="index-heading-more"><a href="<?php bloginfo('url'); ?>/sitemap/popular-posts">View the hit list &#8230;</a></span></h3>
 					
 					<?php if (function_exists('WPPP_show_popular_posts')) {
 						WPPP_show_popular_posts("title=&number=7&days=0&magic_number=1&list_tag=ol&time_format=D, d-M-Y&format=
-												<a href='%post_permalink%' title='%post_title_attribute%'>
-													<span>%post_category%: </span>%post_title%
-													<p>Posted by %post_author% - %post_time% (%post_views% Views)</p>
+												<a  class='wppp-post-title' href='%post_permalink%' title='%post_title_attribute%'>
+													<span class='wppp-post-category'>%post_category%: </span>%post_title%
+													<p class='wppp-post-meta-data'>Posted by %post_author% - %post_time% (%post_views% Views)</p>
 												</a>");
 					} else { ?>
 					<ol class='wppp_list'>
 						<li>
-							<a href='http://www.teda.id.au/mtb/' title='Mountain Biking'>
-								<span>Trail:</span> Cronulla/Kurnell Sand Dunes
-								<p>Posted by Ian - Sat, 30-Jun-2007 (2,574 Views)</p>
+							<a class="wppp-post-title" href='http://www.teda.id.au/mtb/' title='Mountain Biking'>
+								<span class="wppp-post-category">Trail:</span> Cronulla/Kurnell Sand Dunes
+								<p class="wppp-post-meta-data">Posted by Ian - Sat, 30-Jun-2007 (2,574 Views)</p>
 							</a>
 						</li>
 						<li>
-							<a href='http://www.teda.id.au/mtb/' title='Mountain Biking'>
-								<span>Blog: </span>Mountain Biking
-								<p>Posted by Ian - Sat, 30-Jun-2007 (2,574 Views)</p>
+							<a class="wppp-post-title" href='http://www.teda.id.au/mtb/' title='Mountain Biking'>
+								<span class="wppp-post-category">Blog: </span>Mountain Biking
+								<p class="wppp-post-meta-data">Posted by Ian - Sat, 30-Jun-2007 (2,574 Views)</p>
 							</a>
 						</li>
 						<li>
-							<a href='http://www.teda.id.au/photo/rubiks-cube-3x3-%e2%80%93-how-to-solve-in-6-seconds/' title='Rubik&#039;s Cube 3x3 – 6 Second Solve'>
-								<span>Photoblog: </span>Rubik&#039;s Cube 3x3 – 6 Second Solve
-								<p>Posted by Ian - Wed, 23-Apr-2008 (1,421 Views)</p>
+							<a class="wppp-post-title" href='http://www.teda.id.au/photo/rubiks-cube-3x3-%e2%80%93-how-to-solve-in-6-seconds/' title='Rubik&#039;s Cube 3x3 – 6 Second Solve'>
+								<span class="wppp-post-category">Photoblog: </span>Rubik&#039;s Cube 3x3 – 6 Second Solve
+								<p class="wppp-post-meta-data">Posted by Ian - Wed, 23-Apr-2008 (1,421 Views)</p>
 							</a>
 						</li>
 						<li>
-							<a href='http://www.teda.id.au/trail/lady-carrington-drive/' title='Lady Carrington Drive'>
-								<span>Trail: </span>Lady Carrington Drive
-								<p>Posted by Ian - Tue, 31-Jul-2007 (791 Views)</p>
+							<a class="wppp-post-title" href='http://www.teda.id.au/trail/lady-carrington-drive/' title='Lady Carrington Drive'>
+								<span class="wppp-post-category">Trail: </span>Lady Carrington Drive
+								<p class="wppp-post-meta-data">Posted by Ian - Tue, 31-Jul-2007 (791 Views)</p>
 							</a>
 						</li>
 						<li>
-							<a href='http://www.teda.id.au/trail/trail-woronora-bridge-loop/' title='Woronora Bridge Loop'>
-								<span>Trail: </span>Woronora Bridge Loop
-								<p>Posted by Ian - Tue, 19-Feb-2008 (720 Views)</p>
+							<a class="wppp-post-title" href='http://www.teda.id.au/trail/trail-woronora-bridge-loop/' title='Woronora Bridge Loop'>
+								<span class="wppp-post-category">Trail: </span>Woronora Bridge Loop
+								<p class="wppp-post-meta-data">Posted by Ian - Tue, 19-Feb-2008 (720 Views)</p>
 							</a>
 						</li>
 					</ol>
 					<?php }; ?>
 				</div><!-- end #popular-posts-container -->
 				
-				<div id="instagram-container" class="sixCol last clearfix">
+				<div id="index-photo-matrix" class="sixCol last clearfix">
                 	<h3 class="index-heading">Photo Blog<span class="index-heading-more"><a href="<?php bloginfo('url'); ?>/category/photo/">View the matrix &#8230;</a></span></h3>
                 
                 	<ul id="photo-matrix"  class="image-matrix">
